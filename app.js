f5b187083cf221339eef09e2c8ca3cec77765adf
AmenaRimi5599 <amenarimi5@gmail.com>
2024-05-01 23:20:37 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express"); 
+
+const cors = require('cors');
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
